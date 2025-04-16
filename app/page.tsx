f7b22fea2b2bf9b72aeebd64058f2036c71a50a2
YukiNeko <yukineko2233@outlook.com>
2025-04-17 01:28:40 +0800

    bg-fixed

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import GameBoard from "@/components/game-board"
 
 export default function Home() {
     return (
-        <main className="min-h-screen py-12 px-6 bg-[url('/bg.png')] bg-cover bg-center bg-no-repeat flex flex-col items-center justify-center">
+        <main className="min-h-screen py-12 px-6 bg-[url('/bg.png')] bg-cover bg-center bg-fixed bg-no-repeat flex flex-col items-center justify-center">
             <div className="w-full max-w-6xl min-h-[800px]">
                 <GameBoard />
             </div>
