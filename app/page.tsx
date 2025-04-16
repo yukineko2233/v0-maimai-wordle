c2c0649573bef54163032175b2222d66ade64bc8
YukiNeko <yukineko2233@outlook.com>
2025-04-17 00:31:56 +0800

    bg

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
 import GameBoard from "@/components/game-board"
 
 export default function Home() {
-  return (
-    <main className="min-h-screen py-12 px-6 bg-gradient-to-b from-pink-50 to-purple-50 flex flex-col items-center justify-center">
-      <div className="w-full max-w-6xl min-h-[800px]">
-        {/* Increased height with min-h-[800px] */}
-        <GameBoard />
-      </div>
-    </main>
-  )
+    return (
+        <main className="min-h-screen py-12 px-6 bg-[url('/bg.png')] bg-cover bg-center bg-no-repeat flex flex-col items-center justify-center">
+            <div className="w-full max-w-6xl min-h-[800px]">
+                <GameBoard />
+            </div>
+        </main>
+    )
 }
