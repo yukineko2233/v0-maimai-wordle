744757a14657af15c155cc242e7319ff08a18be2
YukiNeko <yukineko2233@outlook.com>
2025-04-24 17:59:27 +0800

    fix room status margin-bottom

--- components/room-status.tsx
+++ components/room-status.tsx
@@ -28,7 +28,7 @@ export default function RoomStatus() {
 
     if (isLoading) {
         return (
-            <div className="text-center text-gray-500 text-sm mb-6 ml-2 mr-2">
+            <div className="text-center text-gray-500 text-sm mb-2 ml-2 mr-2">
                 <Users className="inline h-4 w-4 mr-1" />
                 加载房间数据中，若无响应请等待1分钟左右...
             </div>
